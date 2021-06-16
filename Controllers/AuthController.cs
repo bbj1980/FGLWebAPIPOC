56104a0a05f96992ef2811c087b853aeed98e3f6
bbj1980 <bbj1980@gmail.com>
2021-06-17 00:49:58 +0530

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FGLWebAPIPOC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("GetConfigurationData")]
+        public ActionResult<IEnumerable<Configuration>> GetConfiguration()
+        {
+            return new[]
+            {
+            new Configuration { Key = "Key" ,Value="Value"},
+
+        };
+        }
+        public class Configuration
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
+
+    }
+}
